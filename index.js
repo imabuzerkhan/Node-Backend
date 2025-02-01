07175d89561f5cadb93e8ea5f22804181a5a55c5
imabuzerkhan <abuzerk41@gmail.com>
2025-02-01 19:13:27 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express") ;
+require('dotenv').config()
+const app = express();
+const port = 3000;
+
+const student = {
+  "name" : "abuzer" ,
+  "age" : "24" ,
+ 
+
+  
+}
+
+app.get('/', (req , res)=>{
+res.send("hello world")
+})
+// how to handle json data 
+app.get('/Json', (req , res)=>{
+  res.json(student)
+})
+
+app.get('/home' , (req , res)=>{
+res.send("<h1>Hello i am a home and i am from biratnagar nepal</h1>")
+})
+
+
+app.get('/contact', (req , res)=>{
+  res.send('<span>+9779804395755</span>')
+})
+
+app.listen(process.env.PORT , ()=>{
+  console.log( `connect to backend ${process.env.PORT}`)
+})
