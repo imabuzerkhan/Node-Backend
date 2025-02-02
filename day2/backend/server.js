1f38580d90ff473ec462103025e7ffee34dbdb5d
imabuzerkhan <abuzerk41@gmail.com>
2025-02-02 19:10:53 +0545

    backend work completed

---
+++ day2/backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express' ;
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.get('/api/jokes' , (req , res)=>{
+  const jokes = [
+    {
+      id : 1 ,
+      title : "nice jokes"
+    },
+    {
+        id : 2 ,
+      title : "nice jokes 2"
+    },
+    {
+      id : 3 ,
+    title : "nice jokes 3"
+  },
+  {
+    id : 4 ,
+  title : "nice jokes 4"
+},
+  ]
+  res.send(jokes)
+})
+
+
+app.listen(port , ()=>{
+  console.log(`this server is start on ${port}`)
+})
