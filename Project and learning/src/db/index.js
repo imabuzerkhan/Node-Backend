c8f5af86f88fe0ec76b450dbef5edb3d95bc53e4
imabuzerkhan <abuzerk41@gmail.com>
2025-02-07 19:21:06 +0545

    database connected successfully

---
+++ Project and learning/src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+
+const DatabaseConnect = async()=>{
+try{
+    const connectInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    console.log(`/n mongodb coonected ${connectInstance.connection.host} `)
+} catch(err){
+  console.log("connection failed" , err)
+  process.exit(1)
+}
+}
+
+export default DatabaseConnect
--- Project and learning/src/index.js
+++ Project and learning/src/index.js
@@ -1,25 +1,60 @@
-import mongoose from 'mongoose' 
-import { DB_NAME } from './constant.js'
-import express from 'express' 
+// import mongoose from 'mongoose' 
+// import { DB_NAME } from './constant.js'
+// import express from 'express' 
 import 'dotenv/config'
 
-// dotenv.config({
-//   path : './env'
-// })
+import DatabaseConnect from "./db/index.js";
 
-const app = express();
-( async ()=>{
-     try{
-      await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-      app.on("error" , (err)=>{
-        console.log("error in try block " , err)
-        throw err;
-      }) 
-      app.listen(process.env.PORT , ()=>{
-        console.log(`database connected succesfully ${process.env.PORT}`)
-      })
-     } 
-     catch(err){
-     console.error("connection failed" , err)
-     }
-})()
+
+DatabaseConnect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+// ( async ()=>{
+//      try{
+//       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//       app.on("error" , (err)=>{
+//         console.log("error in try block " , err)
+//         throw err;
+//       }) 
+//       app.listen(process.env.PORT , ()=>{
+//         console.log(`database connected succesfully ${process.env.PORT}`)
+//       })
+//      } 
+//      catch(err){
+//      console.error("connection failed" , err)
+//      }
+// })()
