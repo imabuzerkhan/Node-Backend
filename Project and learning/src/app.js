2289d24fdfb28941af33f376635f8817c053aa0f
imabuzerkhan <abuzerk41@gmail.com>
2025-02-09 19:24:33 +0545

    adding cors and cookies in backend

--- Project and learning/src/app.js
+++ Project and learning/src/app.js
@@ -1,5 +1,24 @@
 import express from 'express'
-
+import cookieParser
+ from 'cookie-parser'
+ import cors from 'cors'
 const app = express()
+
+
+app.use(cors({
+  origin : process.env.CORS_ORIGIN ,
+  credentials : true ,
+})) 
+// josn data accept karne ke liye 
+app.use(express.json({limit:"16kb"}))
+// url ke liye configuration kar rahe hai
+app.use(express.urlencoded({extended:true , limit: "16kb"})) 
+// agar koi pdf wagera ya image save karwana oh to apne server pe hi kar sakte hai
+app.use(express.static("public")) 
+
+
+ 
+
+
 
 export {app}
